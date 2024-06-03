bfa91f725dec149e6b48dae1662b92163f0597ba
dayvidwhy <dayvidwhy@gmail.com>
2024-06-03 12:23:11 +1000

    Commit vscode settings for autolint on save. Upgrade storybook. Shift tailwind config to .ts file.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,33 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
     stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
     core: {
         disableTelemetry: true
     },
+
     addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@chromatic-com/storybook",
-        "@storybook/addon-interactions",
+        getAbsolutePath("@storybook/addon-links"),
+        getAbsolutePath("@storybook/addon-essentials"),
+        getAbsolutePath("@chromatic-com/storybook"),
+        getAbsolutePath("@storybook/addon-interactions"),
     ],
+
     framework: {
-        name: "@storybook/react-vite",
+        name: getAbsolutePath("@storybook/react-vite"),
         options: {},
     },
-    docs: {
-        autodocs: "tag",
-    },
+
+    docs: {},
+
+    typescript: {
+        reactDocgen: "react-docgen-typescript"
+    }
 };
 export default config;
+
+function getAbsolutePath(value: string): any {
+    return dirname(require.resolve(join(value, "package.json")));
+}
